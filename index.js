31674fdd2d61f24e83ae685253c3719cdd57c696
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2022-09-20 22:36:43 -0300

    lixao primario

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
-var img = new Image();
-img.crossOrigin = 'anonymous';
-img.src = 'imgs/nature.jpg';
+let img = new Image();
+img.crossOrigin = "anonymous";
+img.src = "imgs/king-adriano.jpg";
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+let canvasEsq = document.getElementById("canvas-esq");
+let canvasDir = document.getElementById("canvas-dir");
+let contextoRenderingEsq = canvasEsq.getContext("2d");
+let contextoRenderingDir = canvasDir.getContext("2d");
 
-img.onload = function () {
-	ctx.drawImage(img, 0, 0);
-};
+/** @param {CanvasRenderingContext2D} contextoParaDesenhar */
+const mostraImagemInvertida = (contextoParaDesenhar) => {
+	const imageData = contextoRenderingEsq.getImageData(0, 0, canvasEsq.width, canvasEsq.height);
+	const data = imageData.data;
 
-var original = function () {
-	ctx.drawImage(img, 0, 0);
-};
-
-var media = function (pixels, size, colors) {
-
+	for (let i = 0; i < data.length; i += 4) {
+		data[i]     = 255 - data[i];
+		data[i + 1] = 255 - data[i + 1];
+		data[i + 2] = 255 - data[i + 2];
+		data[i + 3] = 255;
+	}
+	contextoParaDesenhar.putImageData(imageData, 0, 0);
 }
 
-var sobel = function () {
-	ctx.drawImage(img, 0, 0);
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+img.onload = function () {
+	canvasEsq.width  = canvasDir.width  = img.width;
+	canvasEsq.height = canvasDir.height = img.height;
+	
+	contextoRenderingEsq.drawImage(img, 0, 0);
+	
+	mostraImagemInvertida(contextoRenderingDir);
+};
+
+
+////// ABAIXO DAQUI SÓ LIXO QUE TEM QUE MUDAR DO LUGAR Q NOIS COPIAMOS KK
+const original = () => {
+	contextoRenderingEsq.drawImage(img, 0, 0);
+};
+
+const sobel = () => {
+	contextoRenderingEsq.drawImage(img, 0, 0);
+	const imageData = contextoRenderingEsq.getImageData(0, 0, canvasEsq.width, canvasEsq.height);
 	const data = imageData.data;
 
 	gradient_internal(imageData, [1, 2, 1]); // Apply Sobel operator
-	ctx.putImageData(imageData, 0, 0);
+	contextoRenderingEsq.putImageData(imageData, 0, 0);
+}
+
+sobel();
+function gradient_internal(pixels, mask) {
+	let data = pixels.data;
+	let w = pixels.width*4;
+	let l = data.length - w - 4;
+	let buff = new data.constructor(new ArrayBuffer(data.length));
+
+	for (let i = w + 4; i < l; i+=4){
+		let dx = conv3x(data, i, w, mask);
+		let dy = conv3y(data, i, w, mask);
+		buff[i] = buff[i + 1] = buff[i + 2] = Math.sqrt(dx*dx + dy*dy);
+		buff[i + 3] = 255;
+	}
+	pixels.data.set(buff);
 }
 
 /**
@@ -32,88 +68,29 @@ var sobel = function () {
 * @param w - image width (width*4 in case of RGBA)
 * @param m - the gradient mask (for Sobel=[1, 2, 1])
 */
-function conv3x(data, idx, w, m){
+function conv3x(data, idx, w, m) {
 	return (m[0]*data[idx - w - 4] + m[1]*data[idx - 4] + m[2]*data[idx + w - 4]
-		-m[0]*data[idx - w + 4] - m[1]*data[idx + 4] - m[2]*data[idx + 4 + 4]);
-  }
-  
-  function conv3y(data, idx, w, m){
+	-m[0]*data[idx - w + 4] - m[1]*data[idx + 4] - m[2]*data[idx + 4 + 4]);
+}
+
+function conv3y(data, idx, w, m) {
 	return (m[0]*data[idx - w - 4] + m[1]*data[idx - w] + m[2]*data[idx - w + 4]
-		-(m[0]*data[idx + w - 4] + m[1]*data[idx + w] + m[2]*data[idx + w + 4]));
-  }
-  
-  
-  /**
-  * @param pixels - Object of image parameters
-  * @param mask - gradient operator e.g. Prewitt, Sobel, Scharr, etc. 
-  */
-  function gradient_internal(pixels, mask)
-  {
-	var data = pixels.data;
-	var w = pixels.width*4;
-	var l = data.length - w - 4;
-	var buff = new data.constructor(new ArrayBuffer(data.length));
-	
-	for (var i = w + 4; i < l; i+=4){
-	  var dx = conv3x(data, i, w, mask);
-	  var dy = conv3y(data, i, w, mask);
-	  buff[i] = buff[i + 1] = buff[i + 2] = Math.sqrt(dx*dx + dy*dy);
-	  buff[i + 3] = 255;
-	}
-	pixels.data.set(buff);
-  }
-  
- 
+	-(m[0]*data[idx + w - 4] + m[1]*data[idx + w] + m[2]*data[idx + w + 4]));
+}
 
-
-var invert = function () {
-	ctx.drawImage(img, 0, 0);
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	const data = imageData.data;
-	for (var i = 0; i < data.length; i += 4) {
-		data[i] = 255 - data[i];     // red
-		data[i + 1] = 255 - data[i + 1]; // green
-		data[i + 2] = 255 - data[i + 2]; // blue
-	}
-	ctx.putImageData(imageData, 0, 0);
-};
-
-var contraste = function () {
-	ctx.drawImage(img, 0, 0);
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	var data = imageData.data;
+const contraste = () => {
+	contextoRenderingEsq.drawImage(img, 0, 0);
+	const imageData = contextoRenderingEsq.getImageData(0, 0, canvasEsq.width, canvasEsq.height);
+	let data = imageData.data;
 
 	let contrast = 128
 
-	var factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+	let factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
 
-	for (var i = 0; i < data.length; i += 4) {
+	for (let i = 0; i < data.length; i += 4) {
 		data[i] = factor * (data[i] - 128) + 128; //red
 		data[i + 1] = factor * (data[i + 1] - 128) + 128; //green
 		data[i + 2] = factor * (data[i + 2] - 128) + 128; //blue
 	}
-	ctx.putImageData(imageData, 0, 0);
+	contextoRenderingEsq.putImageData(imageData, 0, 0);
 };
-
-
-const inputs = document.querySelectorAll('[name=color]');
-console.log(inputs)
-for (const input of inputs) {
-	input.addEventListener("change", function (evt) {
-		switch (evt.target.value) {
-			case "sobel":
-				return sobel();
-			case "inverted":
-				return invert();
-			case "median":
-				return media();
-			case "contrasted":
-				return contraste();
-			default:
-				return original();
-		}
-	});
-}
-
-
-
